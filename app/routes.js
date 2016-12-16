634700d22189e4446a64c95f7d12e4d5dbb0d924
timothylam <lamtimothy1996@gmail.com>
2016-12-15 17:22:34 -0800

    finally got angular working

--- app/routes.js
+++ app/routes.js
@@ -31,7 +31,7 @@ module.exports = function(app) {
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function(req, res) {
-        res.sendfile('./public/views/index.html'); // load our public/index.html file
+        res.sendfile('./public/index2.html'); // load our public/index.html file
     });
 
 };
---
+++ public/js/appRoutes.js
@@ -0,0 +1,19 @@
+// public/js/appRoutes.js
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+    // home page
+        .when('/', {
+            templateUrl: 'views/test.html',
+            controller: 'MainController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/nerds', {
+            templateUrl: 'views/nerd.html',
+            controller: 'NerdController'
+        });
+    $locationProvider.html5Mode(true);
+
+}]);
